bd775fd19b58dffeff53f70315f0c26b8e338357
Juliana Quaresma <julianaqribeiro@hotmail.com>
2025-12-31 13:06:21 -0300

    Inclusao do parametro divider

--- src/types/pipelinesolucoes.ts
+++ src/types/pipelinesolucoes.ts
@@ -22,6 +22,7 @@ export interface PipelineSolucoesButtonTokens {
 
   borderRadius?: string;
   boxShadow?: string;
+  border? : string;
 
   padding?: string;
   margin?: string;
@@ -58,6 +59,7 @@ export interface PipelineSolucoesFormTokens {
     button?: PipelineSolucoesButtonTokens;
     field?: PipelineSolucoesFieldTokens;
     link?: PipelineSolucoesLinkTokens;
+    divider?: string;
   }
 }
 
