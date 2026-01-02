80cdab87a4cf3118ff16cb4ac3b5aad3327c775d
Juliana Quaresma <julianaqribeiro@hotmail.com>
2026-01-02 18:41:11 -0300

    inclusao de novas props no PipelineSolucoesNotificationIconTokens

--- src/types/tokens/notificationTokens.ts
+++ src/types/tokens/notificationTokens.ts
@@ -2,16 +2,27 @@ import { BorderProps } from "../props/BorderProps";
 import { ColorProps } from "../props/ColorProps";
 import { IconProps } from "../props/IconProps";
 import { LayoutProps } from "../props/LayoutProps";
+import { ButtonKind, PipelineSolucoesButtonTokens } from "./buttonTokens";
+import { PipelineSolucoesTypographyTokens } from "./typographyTokens";
 
 export interface PipelineSolucoesNotificationTokens
   extends Pick<ColorProps, 'background' | 'color'>,
   BorderProps,
   LayoutProps {
-
+    
     success?: PipelineSolucoesNotificationIconTokens;
     error?: PipelineSolucoesNotificationIconTokens;
     warning?: PipelineSolucoesNotificationIconTokens;
     info?: PipelineSolucoesNotificationIconTokens;
+    corfim?: PipelineSolucoesNotificationIconTokens;
+
+    buttons?: {
+      primary?: PipelineSolucoesButtonTokens;
+      secondary?: PipelineSolucoesButtonTokens;
+      tertiary?: PipelineSolucoesButtonTokens;
+    }  
+    
+    typography?: PipelineSolucoesTypographyTokens;
   }
 
 export interface PipelineSolucoesNotificationIconTokens
